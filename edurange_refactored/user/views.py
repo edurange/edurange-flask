7097598d254bcdf17338edd2e78982a24e914205
Jack <cookjackc@gmail.com>
2020-02-11 20:57:14 -0800

    modular admin checking

--- edurange_refactored/user/views.py
+++ edurange_refactored/user/views.py
@@ -7,6 +7,11 @@ from .models import User
 
 blueprint = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
 
+def check_admin():
+    number = session.get('_user_id')
+    user = User.query.filter_by(id=number).first()
+    if not user.is_admin:
+        abort(403)
 
 @blueprint.route("/")
 @login_required
@@ -16,9 +21,6 @@ def members():
 
 @blueprint.route("/admin")
 def adminPanel():
-    number = session.get('_user_id')
-    user = User.query.filter_by(id=number).first()
-    if not user.is_admin:
-        abort(403)
+    check_admin()
     return render_template('users/admin.html')
 
