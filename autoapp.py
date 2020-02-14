2fdb5d49d22cef8c6a24b894059b796c8bc6c6ec
Jack <cookjackc@gmail.com>
2020-02-13 20:10:07 -0800

    moved default user creation

--- autoapp.py
+++ autoapp.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
 from edurange_refactored.app import create_app
-
+from edurange_refactored.user.models import User
+import os
 app = create_app()
+app.app_context().push()
+
+def create_admin():
+    username=os.environ['USERNAME']
+    email=os.environ['EMAIL']
+    password=os.environ['PASSWORD']
+    User.create(username=username,
+                email=email,
+                password=password,
+                active=True,
+                is_admin=True,
+                is_instructor=True)
+
+admin = User.query.filter_by(username='admin').first()
+if not admin:
+    create_admin()
+
