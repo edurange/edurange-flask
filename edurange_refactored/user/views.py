bdbbd5f262a9814f1892107df64067c7d6bb2b3e
coojac09 <cookjackc@gmail.com>
2020-04-14 13:05:57 -0700

    Change to dashboard naming convention, new button for students

--- edurange_refactored/user/views.py
+++ edurange_refactored/user/views.py
@@ -9,7 +9,7 @@ from ..utils import StudentTable, Student, GroupTable, Group, GroupUserTable, Gr
 from edurange_refactored.tasks import send_async_email
 from edurange_refactored.extensions import db
 
-blueprint = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
+blueprint = Blueprint("dashboard", __name__, url_prefix="/dashboard", static_folder="../static")
 
 # WARNING:
 # This check is actually vulnerable to attacks.
@@ -26,17 +26,17 @@ def check_admin():
 
 @blueprint.route("/")
 @login_required
-def members():
+def student():
     """List members."""
     db_ses = db.session
     curId = session.get('_user_id')
     userInfo = db_ses.query(User.id, User.username, User.email).filter(User.id == curId)
     infoTable = UserInfoTable(userInfo)
-    return render_template("users/members.html", infoTable=infoTable)
+    return render_template("dashboard/student.html", infoTable=infoTable)
 
 @blueprint.route("/admin", methods=['GET', 'POST'])
 @login_required
-def adminPanel():
+def admin():
     check_admin()
     students = User.query.all()
     stuTable = StudentTable(students)
@@ -49,7 +49,7 @@ def adminPanel():
         form = EmailForm()
         form1 = GroupForm()
         form2 = GroupFinderForm()
-        return render_template('users/admin.html', stuTable=stuTable, groTable=groTable, form=form, form1=form1, form2=form2, groups=groupNames)
+        return render_template('dashboard/admin.html', stuTable=stuTable, groTable=groTable, form=form, form1=form1, form2=form2, groups=groupNames)
     elif request.form.get('to') is not None:
         form = EmailForm(request.form)
         if form.validate_on_submit():
@@ -65,7 +65,7 @@ def adminPanel():
             else:
                 send_async_email.apply_async(args=[email_data], countdown=60)
                 flash('An email will be sent to {0} in one minute.'.format(email))
-            return redirect(url_for('user.adminPanel'))
+            return redirect(url_for('dashboard.admin'))
 
     elif request.form.get('name') is not None:
         form = GroupForm(request.form)
@@ -75,7 +75,7 @@ def adminPanel():
             StudentGroups.create(name=name, owner_id = session.get('_user_id'), code=code)
             flash('Created group {0}'.format(name))
 
-            return redirect(url_for('user.adminPanel'))
+            return redirect(url_for('dashboard.admin'))
 
     elif request.form.get('group') is not None:
         form = GroupFinderForm(request.form)
@@ -84,10 +84,10 @@ def adminPanel():
             name = form.group.data
             groupUsers = db_ses.query(User.id, User.username, User.email, StudentGroups, GroupUsers).filter(StudentGroups.name == name).filter(StudentGroups.id == GroupUsers.group_id).filter(GroupUsers.user_id == User.id)
             groUTable = GroupUserTable(groupUsers)
-            return render_template('users/admin.html', stuTable=stuTable, groTable=groTable, groUTable=groUTable, form=form, groups=groupNames)
+            return render_template('dashboard/admin.html', stuTable=stuTable, groTable=groTable, groUTable=groUTable, form=form, groups=groupNames)
         else:
             flash_errors(form)
-        return redirect(url_for('user.adminPanel'))
+        return redirect(url_for('user.admin'))
 #TODO: add function for adding users to groups
 
     # elif request.form.get('user_group') is not None:
