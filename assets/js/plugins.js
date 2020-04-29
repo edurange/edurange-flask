e9e20e8a8de7f49e89ecf45147851c4189867c47
Adam <adamkover47@gmail.com>
2020-04-29 16:46:55 -0700

    A user can now only be selected once
    If they are selected again, they are removed from the field

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,1 +1,16 @@
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
+//
+
+//function addUser() {
+function addUser(uid)
+	var sel = document.forms['addGroupUsers']['selection'].value;
+	var chk = sel.includes(uid);
+	if (chk == true) {
+	//	document.forms['addGroupUsers']['selection'].value = sel.replace('{{student.id}},', '')
+		document.forms['addGroupUsers']['selection'].value = sel.replace(uid + ',', '');
+	} else {
+		//document.forms['addGroupUsers']['selection'].value += '{{student.id}},'
+		document.forms['addGroupUsers']['selection'].value += uid + ',';
+	}
+}
+
