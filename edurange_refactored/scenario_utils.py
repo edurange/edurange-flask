83950d058281a53cf4334131aeb3aca5f459511a
coojac09 <cookjackc@gmail.com>
2020-07-17 18:44:00 -0700

    Modular Terraform Config Writing
    Scalable, better formatting. Also writes out a json file of students for displaying on the front-end

--- edurange_refactored/scenario_utils.py
+++ edurange_refactored/scenario_utils.py
@@ -1,7 +1,6 @@
 import os
 import yaml
-from flask import session
-from edurange_refactored.user.models import Scenarios
+
 
 class CatalogEntry:
     def __init__(self, name, description):
@@ -10,7 +9,7 @@ class CatalogEntry:
 
 
 def populate_catalog():
-    scenarios = [dI for dI in os.listdir('./scenarios/prod/') if os.path.isdir(os.path.join('./scenarios/prod/',dI))]
+    scenarios = [dI for dI in os.listdir('./scenarios/prod/') if os.path.isdir(os.path.join('./scenarios/prod/', dI))]
     descriptions = []
 
     for s in scenarios:
@@ -25,3 +24,92 @@ def populate_catalog():
     return entries
 
 
+def begin_tf_and_write_providers(name):
+    with open(name + '.tf', 'w') as tf:
+        tf.write(
+            """
+provider "docker" {}
+provider "template" {}
+""")
+
+
+def write_bash(tf, filenames):
+    for f in filenames:
+        tf.write("""
+  provisioner "file" {
+    source      = "${path.module}/../../../scenarios/""" + f + "\"" + "\n" + """
+    destination = """ + "\"/" + f + "\"\n" + """
+  }
+""")
+
+
+def write_users(tf, usernames, passwords):
+    for i, name in enumerate(usernames):
+        tf.write("""
+      "useradd --home-dir /home/""" + name
+            + """ --create-home --shell /bin/bash --password $(echo """ + passwords[i]
+            + """ | openssl passwd -1 -stdin) """ + name + "\","
+                 )
+
+
+def write_run_scripts(tf, filenames):
+    for f in filenames:
+        tf.write(
+            """
+      "chmod +x /""" + f + "\"" + """,
+      "mv /""" + f + " /usr/bin/" + f + "\"" + """,
+"""
+        )
+
+
+def begin_code_block(tf):
+    tf.write(
+        """
+  provisioner "remote-exec" {
+    inline = [
+"""
+    )
+    return
+
+
+def end_code_block(tf):
+    tf.write(
+        """
+    ]   
+  }      
+}
+""")
+
+
+def write_container(name, usernames, passwords, filenames):
+    with open(name + '.tf', 'a') as tf:
+        tf.write(
+            """ 
+resource "docker_container" """ + "\"" + name + "\"" """ {
+  name = """ + "\"" + name + "\"" """
+  image = "rastasheep/ubuntu-sshd:18.04"
+  restart = "always"
+  hostname  = "NAT"
+
+  connection {
+    host = self.ip_address
+    type = "ssh"
+    user = "root"
+    password = "root"
+  }
+
+  ports {
+    internal = 22
+  }
+        """
+        )
+
+        write_bash(tf, filenames)
+
+        begin_code_block(tf)
+
+        write_users(tf, usernames, passwords)
+
+        write_run_scripts(tf, filenames)
+
+        end_code_block(tf)
