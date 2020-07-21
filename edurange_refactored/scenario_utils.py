42c34252bbde946f9d52c8bf41e200f25beddbd6
coojac09 <cookjackc@gmail.com>
2020-07-20 17:20:52 -0700

    Scaffolding for provisioning different filetypes in terraform

--- edurange_refactored/scenario_utils.py
+++ edurange_refactored/scenario_utils.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import json
 
 
 known_types = [
@@ -33,6 +34,69 @@ def populate_catalog():
     for i in range(len(scenarios)):
         entries.append(CatalogEntry(scenarios[i].title(), descriptions[i]))
     return entries
+
+def item_generator(json_input, lookup_key):
+    if isinstance(json_input, dict):
+        for k, v in json_input.items():
+            if k == lookup_key:
+                yield v
+            else:
+                yield from item_generator(v, lookup_key)
+    elif isinstance(json_input, list):
+        for item in json_input:
+            yield from item_generator(item, lookup_key)
+
+
+def gather_files(s_type, logger):
+    c_names = []
+    g_files = []
+    s_files = []
+    u_files = []
+
+    if os.path.isdir(os.path.join('./scenarios/prod/', s_type)):
+        logger.info('Scenario of type {} Found'.format(s_type))
+        logger.info('Now attempting to load file requirements...')
+        try:
+            with open(os.path.join('./scenarios/prod/', s_type + '/' + s_type + '.json')) as f:
+                data = json.load(f)
+
+                containers = item_generator(data, 'name')
+                for i in (containers):
+                    c_names.append(i)
+
+                logger.info("Found containers: {}".format(c_names))
+
+                global_files = item_generator(data, 'global_files')
+                for g in list(global_files):
+                    g_files.append(g)
+
+                logger.info("Found global files: {}".format(g_files))
+
+                system_files = item_generator(data, 'system_files')
+                for s in list(system_files):
+                    s_files.append(s)
+
+                logger.info("Found system files: {}".format(s_files))
+
+                user_files = item_generator(data, 'user_files')
+                for u in list(user_files):
+                    u_files.append(u)
+
+                logger.info("Found global files: {}".format(u_files))
+
+                return c_names, g_files, s_files, u_files
+
+        except FileNotFoundError:
+            logger.warn('Could Not load json file for type: {}'.format(s_type))
+            raise FileNotFoundError
+
+    else:
+        logger.warn('Invalid Scenario Type - Folder Not Found')
+        raise Exception(f'Could not correctly identify scenario type')
+
+
+
+
 
 def identify_type(form):
     found_type = ''
@@ -101,7 +165,7 @@ def end_code_block(tf):
 """)
 
 
-def write_container(name, usernames, passwords, filenames):
+def write_container(name, usernames, passwords, g_files, s_files, u_files):
     with open(name + '.tf', 'a') as tf:
         tf.write(
             """ 
@@ -124,12 +188,12 @@ resource "docker_container" """ + "\"" + name + "\"" """ {
         """
         )
 
-        write_global_files(tf, filenames)
+        write_global_files(tf, g_files)
 
         begin_code_block(tf)
 
         write_users(tf, usernames, passwords)
 
-        write_run_global(tf, filenames)
+        write_run_global(tf, g_files)
 
         end_code_block(tf)
--- edurange_refactored/tasks.py
+++ edurange_refactored/tasks.py
@@ -5,7 +5,7 @@ from flask_mail import Mail, Message
 from flask import current_app, render_template, session, flash
 from os import environ
 
-from edurange_refactored.scenario_utils import write_container, begin_tf_and_write_providers, known_types
+from edurange_refactored.scenario_utils import write_container, begin_tf_and_write_providers, known_types, gather_files
 from edurange_refactored.settings import CELERY_BROKER_URL, CELERY_RESULT_BACKEND
 import os
 import string
@@ -76,26 +76,7 @@ def CreateScenarioTask(self, name, s_type, owner, group):
     from edurange_refactored.user.models import Scenarios
     app = current_app
     s_type = s_type.lower()
-    containers = {}
-
-
-    if os.path.isdir(os.path.join('./scenarios/prod/', s_type)):
-        logger.info('Scenario of type {} Found'.format(s_type))
-        logger.info('Now attempting to load file requirements...')
-        try:
-            with open(os.path.join('./scenarios/prod/', s_type + '/' + s_type + '.json')) as f:
-                data = json.load(f)
-                for i in data['containers']:
-                    logger.info('Identified Container: {} '.format(i))
-                    containers[i['name']] = i['name']
-
-        except FileNotFoundError:
-            logger.warn('Could Not load json file for type: {}'.format(s_type))
-            raise FileNotFoundError
-
-    else:
-        logger.warn('Invalid Scenario Type - Folder Not Found')
-        raise Exception(f'Could not correctly identify scenario type')
+    c_names, g_files, s_files, u_files = gather_files(s_type, logger)
 
     logger.info('Executing task id {0.id}, args: {0.args!r} kwargs: {0.kwargs!r}'.format(
         self.request))
@@ -137,9 +118,8 @@ def CreateScenarioTask(self, name, s_type, owner, group):
 
         begin_tf_and_write_providers(name)
 
-        filenames = ['iamfrustrated']
-
-        write_container(name, usernames, passwords, filenames)
+        for i, c in enumerate(c_names):
+            write_container(name + '_' + c, usernames, passwords, g_files[i], s_files[i], u_files[i])
 
 
         os.system('terraform init')
