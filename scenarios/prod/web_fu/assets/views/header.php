2a8cb7ca2e9f4b267a7e69da71a244d617c75c7a
Carlos García Morán <carlos@MacBuk-Pro.local>
2021-04-13 15:50:55 -0700

    webfu: add support for ssl; bump bootstrap version

--- scenarios/prod/web_fu/assets/views/header.php
+++ scenarios/prod/web_fu/assets/views/header.php
@@ -1,5 +1,5 @@
 <head>
   <meta charset="utf-8">
   <title>Level <?= LEVEL ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
