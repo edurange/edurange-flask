5f2544eaaf5ddf3a378e1cded2e29b40160ed71e
carlosgmoran <46545682+carlos-garciamoran@users.noreply.github.com>
2022-03-10 15:12:27 -0800

    webfu: add stored XSS lab; add XSS questions; update Bootstrap

--- scenarios/prod/web_fu/assets/views/header.php
+++ scenarios/prod/web_fu/assets/views/header.php
@@ -1,5 +1,5 @@
 <head>
   <meta charset="utf-8">
-  <title>Level <?= $level ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <title>WebFu | Level <?= $level ?></title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
