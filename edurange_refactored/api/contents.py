2720aeb508ddf2a871aa512756ec875e40daea62
lovercast <levi.overcast@gmail.com>
2022-07-12 20:25:29 +0000

    Fix state behavior for multi-string questions

--- edurange_refactored/api/contents.py
+++ edurange_refactored/api/contents.py
@@ -1,4 +1,6 @@
 """Public section, including homepage and signup."""
+from functools import reduce
+from logging import raiseExceptions
 from flask import (
     Blueprint,
     current_app,
@@ -32,7 +34,7 @@ from edurange_refactored.public.forms import (
 from edurange_refactored.tasks import test_send_async_email
 from edurange_refactored.user.forms import RegisterForm
 from edurange_refactored.user.models import GroupUsers, StudentGroups, User
-from edurange_refactored.utils import TokenHelper, calcScr, displayProgress, flash_errors, getAttempt, getTotalScore, questionReader, scoreCheck, scoreSetup, tempMaker
+from edurange_refactored.utils import TokenHelper, bashAnswer, calcScr, displayProgress, flash_errors, getAttempt, getTotalScore, questionReader, scoreCheck, scoreSetup, tempMaker
 
 blueprint = Blueprint("api", __name__, url_prefix="/api", static_folder="../static")
 jwtToken = JWT()
@@ -110,7 +112,6 @@ def post_ans(i):
 
 def calc_state(user_id:          int,
                scenario_id:      int):
-    score = 0
     db_ses = db.session
     query = db_ses\
         .query(Scenarios.name, Scenarios.attempt)\
@@ -118,7 +119,7 @@ def calc_state(user_id:          int,
         .first()
     scenario_name, current_attempt = query.name, query.attempt 
     query = db_ses\
-        .query(Responses.points, Responses.question)\
+        .query(Responses.points, Responses.question, Responses.student_response)\
         .filter_by(scenario_id=scenario_id)\
         .filter_by(user_id=user_id)\
         .filter_by(attempt=current_attempt)\
@@ -127,22 +128,14 @@ def calc_state(user_id:          int,
     # score per questions, total score so far, most recent result -- correct or incorrect
     State = {"CurrentScore" : 0}
     Questions = {}
-    """
-    {
-        "CurrentScore" : int,
-        "Questions" : {
-            "1" : {
-                "Score" : int,
-                "Correct" : bool
-            }, 
-            "2" : {
-                "Score" : int, 
-                "Correct" : bool
-            }
-        }
-    }
-    """
-    checkList = scoreSetup(questionReader(scenario_name))
+    questions_yml = questionReader(scenario_name)
+    # check for interpolations and make substitution
+    for question in questions_yml:
+        for ans in question["Answers"]:
+            if type(ans["Value"]) == str and "${" in ans["Value"]:
+                ans["Value"] = bashAnswer(scenario_id, user_id, ans["Value"])
+
+    checkList = score_setup(questions_yml)
     for row in query:
         # create row and set the most recent result
         points = int(row.points)
@@ -153,7 +146,8 @@ def calc_state(user_id:          int,
             }
         # if the score is passing, check it off and update state
         if points > 0:
-            check, checkList = scoreCheck(row.question, checkList)
+            check, checkList = score_check(row.question, row.student_response, checkList)
+            # raise Exception("custom breakpoint")
             if not check:
                 Questions[row.question]["Score"] += points
                 State["CurrentScore"] += points
@@ -167,3 +161,67 @@ def scenario_supports(scenario_id: int):
         return scenario.description.lower() in [ # in scenarios which have updated student views
             "getting_started"
         ]
+
+def score_check(qNum: int, 
+                response: str,
+                checkList):
+    """
+    Description: Mark a response graded in the checklist so that the grade is computed correctly.
+    Input:  Question number, 
+            Student response
+            Checklist object.
+    Output: isChecklistUnchanged, bool
+            New Checklist, object
+            givePoints, bool
+    """
+    # resp_key = canonicalize_response(response)  
+    # resp_key = canonicalize_response(
+    #             scenario_id,
+    #             user_id,
+    #             response
+
+    #         )
+    # in case of need to canonicalize, wrap the response
+    resp_key = response
+    if type(checkList[qNum]) == dict: ## multi-part question
+        # if all already checked or answer already in responses, 
+        # don't change, don't give points
+        if (checkList[qNum]["Tasks"] == 0 
+                or resp_key in checkList[qNum]["Responses"]):
+            return True, checkList
+        else:
+            checkList[qNum]["Responses"].append(resp_key)
+            checkList[qNum]["Tasks"] -= 1
+            return False, checkList
+    else:
+        if checkList[qNum]:
+            return True, checkList  # answer has already been checked
+        elif not checkList[qNum]:
+            checkList[qNum] = True
+            return False, checkList  # answer has not been checked before but is now checked
+
+
+
+def score_setup(questions):
+    checkList = {}  # List of question to be answered so duplicates are not scored.
+
+    for index, question in enumerate(questions):
+        qNum = index + 1
+        if question['Type'] == "Multi String":
+            checkList[qNum] = {
+                "Tasks":len(question["Answers"]),
+                "Responses":[]
+            }
+        else:
+            checkList[qNum] = False
+
+    return checkList
+
+def is_correct(student_response: str, answer: str):
+    return student_response == answer or answer == "ESSAY"
+
+def canonicalize_response(scenario_id: int,
+    user_id: int,
+    student_response: str,
+    answer: str):
+    return student_response
