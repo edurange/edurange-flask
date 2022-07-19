20a95f244dc476d11df8fd651c7cab27e453f1d5
KD0AZT <vagrant@ubuntu2204.localdomain>
2022-07-19 20:14:20 +0000

    add guidebox React component and fix React rendering

---
+++ edurange_refactored/templates/student_view/partials/guidebox.js
@@ -0,0 +1,10 @@
+export class Guidebox extends React.Component {
+    render() {
+        return (
+            <div className='guidebox'>
+                <h3>Guidebox goes here</h3>
+                <p>More guidebox stuff here</p>
+            </div>
+        );
+    }
+}
--- edurange_refactored/templates/student_view/partials/student_view_question.js
+++ edurange_refactored/templates/student_view/partials/student_view_question.js
@@ -1,37 +1,48 @@
-'use strict';
 import sample_content from './sample_content.json';
-console.log(sample_content);
-let submission = 'unsubmitted';
+import { Guidebox } from './guidebox.js';
 let answer = sample_content.StudentGuide.Questions.Question1.Answers.Value;
 
 class Question extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      submission: 'unsubmitted'
+    }
+    this.updateSubmit = this.updateSubmit.bind(this);
+  }
+
+  updateSubmit() {
+    this.setState(
+      {
+        submission: 'submitted'
+      }
+    )
+  }
+
+
 
   render() {
 
     return (
       <div className="question">
         <h1 className='scenario_name'>{sample_content.ScenarioTitle}</h1>
-        <form className="question--form">
-                  <div className="submission-traits">
-                      <p className="submission-points">{sample_content.StudentGuide.Questions.Question1.Points} points</p>
-                      <p className="submission-state">{submission}</p>
-                  </div>
-          <p className="question-text">{sample_content.StudentGuide.Questions.Question1.Text}</p>
-
-
-          <textarea className="answer" rows="1" cols="50"></textarea><br />
-          <input className='submit' type="submit" value="Submit" />
-        </form>
-        <div className="guide">
-          <ul className="guide_list">
-            <li>Number of Questions: {sample_content.StudentGuide.TotalQuestions}</li>
-            <li>Number of Points: {sample_content.StudentGuide.TotalPoints}</li>
-          </ul>
+        <div className="submission-traits">
+          <p className="submission-points">{sample_content.StudentGuide.Questions.Question1.Points} points</p>
+          <p className="submission-state">{this.state.submission}</p>
         </div>
+        <p className="question-text">{sample_content.StudentGuide.Questions.Question1.Text}</p>
+        <div class='answer-area'>
+          <textarea className="answer" rows="1" cols="50"></textarea><br />
+          <button className='submit' type="submit" onClick={this.updateSubmit}>Submit</button>
+        </div>
+        <Guidebox className='guidebox' />
       </div>
       
     );
   }
 }
+
+
 
 ReactDOM.render(<Question />, document.getElementById('student_view_question'))
