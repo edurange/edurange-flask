0fa2fbfed72ddc9c4c824edc10e60d21ff345bc7
KD0AZT <vagrant@ubuntu2204.localdomain>
2022-07-20 21:19:48 +0000

    removed redundant files, moved to components.

--- edurange_refactored/templates/student_view/partials/chatbox.js
+++
@@ -1,9 +0,0 @@
-export class Chatbox extends React.Component {
-    render() {
-        return (
-            <a href='student_view' className='chatbox'>
-                <p>How can we help?</p>
-            </a>
-        );
-    }
-}
