00f420eac55dd4ae0af42124d91bf4de4a61634e
kd0azt <kd0azt@gmail.com>
2022-09-20 15:38:54 +0000

    Add chat message section react base code

--- edurange_refactored/templates/instructor_view/components/chat_window/chat_window.component.js
+++ edurange_refactored/templates/instructor_view/components/chat_window/chat_window.component.js
@@ -1,11 +1,15 @@
 /* Display one chat session.
  */
 import "./chat_window.css"
+import ChatMessageSection from "../chat_message_section/chat_message_section.component";
 
 class ChatWindow extends React.Component {
     render() {
+        const user = this.props;
         return (
-            <div id='chat'>Chat Area</div>
+            <div id='chat'>
+                <ChatMessageSection user={user}/>
+            </div>
         );
     }
 }
