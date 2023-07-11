2d583ac12b64e62d1a33bb4cf4a0f3a510043716
whereismyprozac <bunnyhpppy3@gmail.com>
2023-07-11 02:03:15 +0000

    student veiw

--- edurange_refactored/templates/student_veiw_/student_veiw.js
+++ edurange_refactored/templates/student_veiw_/student_veiw.js
@@ -1,45 +1,41 @@
-// Filename: student_veiw.js
-import "./student_veiw.css";
-import {createRoot} from 'react-dom/client';
-import React, { useState, useEffect } from 'react';
+import React, { Component, useEffect } from 'react';
+import './student_veiw.css';
 
-function Student(props) {
-    // const [selectedStudent, setSelectedStudent] = useState();
+function Student({ scenario_id }){
+    useEffect(() => {
+        //script
+        const script = document.createElement('script');
+        script src="/static/build/student_scenario.bundle.js";
+        script.async = true;
+        document.body.appendChild(script);
 
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
     return (
-<div id="student_veiw"> 
-    <h1 class="mt-5">Student</h1>
-
-{% extends "layout.html" %}
-{% block content %}
-    <link
-            rel="stylesheet"
-            type="text/css"
-            href="{{ url_for('static', filename='build/student_scenario.bundle.css') }}"
-    />
-<main class="student">
-  <div id="student_scenario" scenario_id={{scenario_id}}></div>
-</div>
-</main>
-
-{% endblock %}
-
-{% block  js %}
-<!-- development React scripts !!Replace with production scripts (below) for release!! -->
-<!-- script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script
-<script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script> -->
-
-<!-- production React scripts -->
-<!-- <script src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
-<script src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script> -->
-
-<script src="/static/build/student_scenario.bundle.js"></script>
-{% endblock %}
-
-</div>
+        <div>
+            <link rel="stylesheet" href="/static/build/student_scenario.css" />
+            /&gt;
+            <main className='student'>
+                <div id="student_scenario" scenario_id={scenario_id}></div>
+            </main>
+        </div>
     );
-}   
+}
+export default Student;
+import React from 'react';
+import Student from './student_veiw.js';
+
+function App() {
+    return <div>
+        <Student scenario_id={scenario_id} />
+        <Student scenario_id="123"/>
+       
+        </div>
+    
+}
 var e = document.getElementById('student_veiw');
 const root=createRoot(e);
 
-root.render(<About/>);
+root.render(<Student/>);
