d0886102386c026c1d7f63e507dd74e06d0fc8a9
Riles <wrilesb@gmail.com>
2023-07-11 19:03:01 +0000

    Pushed Account Management HTML and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -86,6 +86,55 @@ module.exports = [
 
   },
   {
+   context: path.join(__dirname, '/edurange_refactored/templates/accountmgmt/components/'),
+   entry: {
+     accountmgmt: './accountmgmt',
+   },
+   output: {
+     chunkFilename: "[id].js",
+     filename: "[name].bundle.js",
+     path: path.join(__dirname, "edurange_refactored", "static", "build"),
+     publicPath: "/static/build/",
+     library: "lib",
+     libraryTarget: "var"
+
+
+   },
+   resolve: {
+     extensions: [".js", ".jsx", ".css"]
+   },
+   plugins: [
+     new MiniCssExtractPlugin({ filename: "[name].bundle.css" }),
+   ],
+   module: {
+     rules: [
+       {
+         test: /\.?js(x)?$/,
+         use: {
+           loader: "babel-loader",
+           options: {
+             presets: ['@babel/preset-env', '@babel/preset-react']
+           }
+         },
+       },
+       {
+         test: /\.css$/,
+         use: [
+           {
+             loader: MiniCssExtractPlugin.loader,
+             options: {
+               hmr: debug,
+             },
+           },
+           'css-loader',
+         ],
+       }
+     ]
+   }
+ },
+
+	{
+		
     context: path.join(__dirname, '/edurange_refactored/templates/student_view/components/'),
     entry: {
       student_scenario: './scenario/scenario.component',
